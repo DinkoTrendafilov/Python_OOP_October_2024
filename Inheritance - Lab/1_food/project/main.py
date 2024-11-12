632e4a25f8f6e0a41a33d15dd1235d3f4c4b7235
Dinko Delchev Trendafilov <32400715+DinkoTrendafilov@users.noreply.github.com>
2024-11-12 12:33:56 +0200

    Add files

---
+++ Inheritance - Lab/1_food/project/main.py
@@ -0,0 +1,9 @@
+from project.fruit import Fruit
+
+a = Fruit("apple", "2024-02-06")
+b = Fruit("banana", "2023-12-25")
+
+print(a.name)
+print(a.expiration_date)
+print(b.name)
+print(b.expiration_date)
