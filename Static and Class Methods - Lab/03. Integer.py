c8c8f9e18f6149f12cffb13b8f295fbe7e613bac
Dinko Delchev Trendafilov <32400715+DinkoTrendafilov@users.noreply.github.com>
2024-11-15 10:08:44 +0200

    Create 03. Integer.py

---
+++ Static and Class Methods - Lab/03. Integer.py
@@ -0,0 +1,46 @@
+class Integer:
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        if not isinstance(float_value, float):
+            return "value is not a float"
+        return cls(int(float_value))
+
+    @classmethod
+    def from_roman(cls, value: str):
+        roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        result = 0
+        for el in value:
+            result += roman_numerals[el]
+
+        return cls(int(result))
+
+    @classmethod
+    def from_string(cls, value: str):
+        if not isinstance(value, str):
+            return "wrong type"
+
+        try:
+            return cls(int(value))
+        except ValueError:
+            return "wrong type"
+
+
+first_num = Integer(10)
+print(first_num.value)
+
+second_num = Integer.from_roman("IV")
+print(second_num.value)
+
+print(Integer.from_float("2.6"))
+print(Integer.from_string(2.6))
